5e53e99526c6b3402141bd30b56947b8b54a9361
kdao <khanh.dao@move.com>
2018-07-09 22:17:16 -0700

    Example for hapi rate limit

--- hapi/index.js
+++ hapi/index.js
@@ -3,33 +3,55 @@
 const Hapi=require('hapi');
 
 // Create a server with a host and port
-const server=Hapi.server({
-    host:'localhost',
-    port:8000
+const server = Hapi.server({
+  cache: {
+    engine: require('catbox-memory'),
+    name: 'memory'
+  },
+  host:'localhost',
+  port:8000
 });
 
 // Add the route
 server.route({
-    method:'GET',
-    path:'/hello',
-    handler:function(request,h) {
+  method:'GET',
+  path: '/',
+  handler: async (request, h) => {
+    return 'hello world';
+  }
+});
 
-        return'hello world';
+server.route({
+  method:'GET',
+  path: '/test',
+  config: {
+    plugins: {
+      'hapi-rate-limit': {
+        pathLimit: 2,
+        pathCache: {
+          expiresIn: 60 * 1000 // 1 min
+        }
+      }
+    },
+    handler: async (request, h) => {
+      return 'Testing page';
     }
+  }
 });
 
 // Start the server
-async function start() {
-
-    try {
-        await server.start();
-    }
-    catch (err) {
-        console.log(err);
-        process.exit(1);
-    }
-
-    console.log('Server running at:', server.info.uri);
+const init = async () => {
+  await server.register({
+    plugin: require('hapi-rate-limit'),
+    options: {}
+  });
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
 };
 
-start();
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
